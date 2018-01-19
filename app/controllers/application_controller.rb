40c71e8544dae46453917556d22d7dc23122296a
Andrew Carroll <andrewjcarroll3@gmail.com>
2018-01-19 01:46:35 +0000

    Added HTTP basic auth for admin pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
   protect_from_forgery with: :exception
 
   def current_user
@@ -26,6 +27,13 @@ class ApplicationController < ActionController::Base
       expires: 10.days.from_now
     }
     cookies[:cart]
+  end
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['AUTHENTICATE_USERNAME'] &&
+      password == ENV['AUTHENTICATE_PASSWORD']
+    end
   end
 
 end
